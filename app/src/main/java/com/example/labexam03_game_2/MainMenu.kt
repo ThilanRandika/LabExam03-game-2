c14283911b56422774c3896ccda05d64455fb68c
Thilan Randika <128400496+ThilanRandika@users.noreply.github.com>
2024-04-23 01:59:05 +0530

    background moving

--- app/src/main/java/com/example/labexam03_game_2/MainMenu.kt
+++ app/src/main/java/com/example/labexam03_game_2/MainMenu.kt
@@ -3,12 +3,14 @@ package com.example.labexam03_game_2
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainMenu : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +20,14 @@ class MainMenu : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+
+    //start the game when the play button clicks on the main menu
+    fun startTheGame(v: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
 
 }
